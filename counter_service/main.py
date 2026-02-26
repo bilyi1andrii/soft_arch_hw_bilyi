f38924f4ef130d356a59bf4859cc2a208c37f0e5
Andrii <bilyjandrij4@gmail.com>
2026-02-26 20:08:14 +0200

    Add scripts for steps 4-8

--- counter_service/main.py
+++ counter_service/main.py
@@ -1,54 +1,27 @@
-from hazelcast.client import HazelcastClient
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    client = HazelcastClient(
-        cluster_name="dev-cluster",
-        cluster_members=[
-            "hazelcast1:5701",
-            "hazelcast2:5701",
-            "hazelcast3:5701"
-        ]
-    )
+app = FastAPI()
 
-    app.state.hz_client = client
-    app.state.users_balance = client.get_map("users_balance")
-    yield
-    client.shutdown()
+users_balance = {}
 
-app = FastAPI(lifespan=lifespan)
 
 @app.post("/transaction")
 async def count_user_balance(transaction: dict):
     user_id = transaction["user_id"]
     amount = transaction["amount"]
 
-    hz_map = app.state.users_balance
+    users_balance[user_id] = users_balance.get(user_id, 0) + amount
 
+    print(f"[COUNTER] User '{user_id}' applied {amount}. New balance: {users_balance[user_id]}", flush=True)
 
-    current_balance = await hz_map.get(user_id) or 0
-    new_balance = current_balance + amount
+    return {"balance": users_balance[user_id]}
 
-
-    await hz_map.put(user_id, new_balance)
-
-    print(f"[COUNTER] User '{user_id}' applied {amount}. New balance: {new_balance}", flush=True)
-
-    return {"balance": new_balance}
 
 @app.get("/balance/{user_id}")
 async def get_user_balance(user_id: str):
-    hz_map = app.state.users_balance
-    balance = await hz_map.get(user_id) or 0
-    return {"balance": balance}
+    return {"balance": users_balance.get(user_id, 0)}
+
 
 @app.get("/balance")
 async def get_all_user_balance():
-    hz_map = app.state.users_balance
-
-    balances = await hz_map.entry_set()
-
-    balances_dict = {k: v for k, v in balances}
-    return {"balances": balances_dict}
+    return {"balances": users_balance}
