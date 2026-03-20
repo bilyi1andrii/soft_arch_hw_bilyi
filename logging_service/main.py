fac7943e20ceac14124b045401a3b6df288be5f4
Andrii <bilyjandrij4@gmail.com>
2026-03-20 11:54:25 +0200

    Connect hazelcast to logging

--- logging_service/main.py
+++ logging_service/main.py
@@ -1,16 +1,27 @@
+from hazelcast.client import HazelcastClient
 from fastapi import FastAPI
 
+
 app = FastAPI()
-user_logs = {}
+
+client = HazelcastClient(
+    cluster_name="dev-cluster",
+    cluster_members=[
+        "hazelcast1:5701",
+        "hazelcast2:5701",
+        "hazelcast3:5701"
+    ]
+)
+
+distributed_map = client.get_map("messages_map").blocking()
 
 
 @app.post("/transaction")
 def log_user(transaction: dict):
-
     transaction_id = transaction["transaction_id"]
     user_id = transaction["user_id"]
 
-    user_logs[transaction_id] = transaction
+    distributed_map.put(transaction_id, transaction)
 
     print(f"[LOGGING] Stored transaction {transaction_id} for user '{user_id}'", flush=True)
 
@@ -19,6 +30,7 @@ def log_user(transaction: dict):
 
 @app.get("/transaction/{user_id}")
 async def get_user_transactions(user_id: str):
-    transactions = [elm for elm in user_logs.values() if elm.get("user_id") == user_id]
+    all_logs = distributed_map.values()
+    transactions = [elm for elm in all_logs if elm.get("user_id") == user_id]
 
     return {"transactions": transactions}
